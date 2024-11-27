71c3a313d837adea743c9f5a8c69e50d45a9e081
Pamela Riviere <pdrivier@ucsd.edu>
2024-11-27 15:38:48 -0800

    set up sentences in batch

--- scripts/get-contextualized-embeddings.py
+++ scripts/get-contextualized-embeddings.py
@@ -16,52 +16,35 @@ from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModel
 
 ### Custom functions to ...
-### ... find the target tokens within tokenized sequence
-def find_sublist_index(mylist, sublist):
-    """Find the first occurence of sublist in list.
-    Return the start and end indices of sublist in list"""
-
-    for i in range(len(mylist)):
-        if mylist[i] == sublist[0] and mylist[i:i+len(sublist)] == sublist:
-            return i, i+len(sublist)
-    return None
-
-@functools.lru_cache(maxsize=None)  # This will cache results, handy later...
-
-
-### ... grab the embeddings for your target tokens
-def get_embedding(model, tokenizer, sentence, target, layer, device):
-    """Get a token embedding for target in sentence"""
+def batch_tokenize_sentences(sentences, model_name, device):
+    """
+    Tokenize a batch of sentences using a specified transformer model.
     
-    # Tokenize sentence
-    inputs = tokenizer(sentence, return_tensors="pt").to(device)
+    Args:
+        sentences (list): A list of sentences to tokenize
+        model_name (str): Name of the pretrained tokenizer model (default: 'bert-base-uncased')
     
-    # Tokenize target
-    target_enc = tokenizer.encode(target, return_tensors="pt",
-                                  add_special_tokens=False).to(device)
+    Returns:
+        dict: A dictionary containing tokenization results
+    """
+    # Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     
-    # Get indices of target in input tokens
-    target_inds = find_sublist_index(
-        inputs["input_ids"][0].tolist(),
-        target_enc[0].tolist()
-    )
-
-    # Run model
-    with torch.no_grad():
-        output = model(**inputs,output_hidden_states=True)
-        hidden_states = output.hidden_states
-
-    # Get layer
-    selected_layer = hidden_states[layer][0]
-
-    #grab just the embeddings for your target word's token(s)
-    token_embeddings = selected_layer[target_inds[0]:target_inds[1]]
-
-    #if a word is represented by >1 tokens, take mean
-    #across the multiple tokens' embeddings
-    embedding = torch.mean(token_embeddings, dim=0)
+    # Tokenize the batch of sentences
+    tokenized_batch = tokenizer(
+        sentences, 
+        padding=True,  # Pad sequences to the same length
+        truncation=True,  # Truncate to the model's max length
+        return_tensors='pt'  # Return PyTorch tensors
+    ).to(device)
     
-    return embedding
+    return tokenized_batch, {
+        'input_ids': tokenized_batch['input_ids'],
+        'attention_mask': tokenized_batch['attention_mask'],
+        'tokens': [tokenizer.convert_ids_to_tokens(ids) for ids in tokenized_batch['input_ids']]
+    }
+
+
 
 
 ### TODO: 
@@ -100,7 +83,7 @@ def split_excerpts(list_of_excerpts):
 sentences = split_excerpts(excerpts)
 
 ### TODO: Getting random subset
-random_subset = random.sample(sentences, 1000)
+random_subset = random.sample(sentences, 10)
 
 #####
 #####
@@ -125,26 +108,48 @@ model = AutoModel.from_pretrained(mpath, revision = str(checkpoint)).to(device)
 ##### save the list of target words, with their sentences in a df
 
 
-# Tokenize sentence
-sentence = random_subset[30]
-inputs = tokenizer(sentence, return_tensors="pt", add_special_tokens=False) #don't want to accidentally select special tokens, so tokenize first without them!
-target_token_id = random.sample(inputs["input_ids"].tolist()[0],1)
-target_token = tokenizer.decode(target_token_id)
-inputs = tokenizer(sentence, return_tensors="pt", add_special_tokens=True).to(device) #retokenize with special tokens
-token_sequence = inputs["input_ids"][0].tolist()
-target_sentence_id = [i for i,val in enumerate(token_sequence) if val == target_token_id[0]][0] #use this to find the position of the token index in the sentence
-
-
-# Run model
+# Tokenize the batch of sentences
+inputs, tokenized_sentences = batch_tokenize_sentences(random_subset,mpath,device)
 with torch.no_grad():
     output = model(**inputs,output_hidden_states=True)
     hidden_states = output.hidden_states
 
-# Iterate through layers, and grab the embedding for just the target token per layer
-layer = 4
-# Get layer
-selected_layer = hidden_states[layer][0]
+sentence_lens = []
+for s in tokenized_sentences["input_ids"]: 
+	tmplist = [i for i in s if not i in [50000,50001,50002]]
+	sentence_lens.append(len(tmplist))
+sentence_lens
 
-#grab just the embeddings for your target token
-token_embedding = selected_layer[target_sentence_id:target_sentence_id+1]
+# Print the results
+# print("Input IDs:", result['input_ids'])
+# print("Attention Mask:", result['attention_mask'])
+# print("Tokens:")
+# for tokens in result['tokens']:
+#     print(tokens)
+
+
+
+
+
+# sentence = random_subset[30]
+# inputs = tokenizer(sentence, return_tensors="pt", add_special_tokens=False) #don't want to accidentally select special tokens, so tokenize first without them!
+# target_token_id = random.sample(inputs["input_ids"].tolist()[0],1)
+# target_token = tokenizer.decode(target_token_id)
+# inputs = tokenizer(sentence, return_tensors="pt", add_special_tokens=True).to(device) #retokenize with special tokens
+# token_sequence = inputs["input_ids"][0].tolist()
+# target_sentence_id = [i for i,val in enumerate(token_sequence) if val == target_token_id[0]][0] #use this to find the position of the token index in the sentence
+
+
+# # # Run model
+# with torch.no_grad():
+#     output = model(**inputs,output_hidden_states=True)
+#     hidden_states = output.hidden_states
+
+# # Iterate through layers, and grab the embedding for just the target token per layer
+# layer = 4
+# # Get layer
+# selected_layer = hidden_states[layer][0]
+
+# #grab just the embeddings for your target token
+# token_embedding = selected_layer[target_sentence_id:target_sentence_id+1]
 
